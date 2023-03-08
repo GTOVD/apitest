54940930da7d73babbc56da1a752533fbae48edd
Thomas Vickers <vickerst10@gmail.com>
2023-03-07 18:13:14 -0700

    fix database8

--- api/users.js
+++ api/users.js
@@ -3,7 +3,8 @@ import { connectToDatabase } from "../lib/database";
 module.exports = async (req, res) => {
     if (req.method === "GET") {
         const db = await connectToDatabase();
-        const collection = db.collection("movies");
+        const database = db.db("sample_mflix");
+        const collection = database.collection("movies");
         const users = await collection.find({}).toArray();
 
         res.status(200).json({ users });
