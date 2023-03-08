b28c9810efec1db1af65da416c80529ca9d5a068
Thomas Vickers <vickerst10@gmail.com>
2023-03-08 12:05:48 -0700

    fix database10

--- api/users.js
+++ api/users.js
@@ -5,15 +5,17 @@ const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 module.exports = async (req, res) => {
-    if (req.method === "GET") {
-        const database = client.db("sample_mflix");
-        const getMovies = database.collection("movies");
-        const movies = await getMovies.find({}).toArray();
-        res.status(200).json({ movies });
+    try {
+        if (req.method === "GET") {
+            const database = client.db("sample_mflix");
+            const getMovies = database.collection("movies");
+            const movies = await getMovies.find({}).toArray();
+            res.status(200).json({ movies });
+        } else {
+            const { name, location } = req.body;
+            res.send({ status: "User Created", name, location });
+        }
+    } finally {
         await client.close();
-    } else {
-        const { name, location } = req.body;
-
-        res.send({ status: "User Created", name, location });
     }
 };
