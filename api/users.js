0b0110aa6f051f9e2a601bdcee0d1b190c5fcd83
Thomas Vickers <vickerst10@gmail.com>
2023-03-08 12:44:28 -0700

    fix database13

--- api/users.js
+++ api/users.js
@@ -1,9 +1,9 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
-// const uri = process.env.MONGODB_URI;
-const uri =
-    "mongodb+srv://vercel-admin-user:H6iHPF1uS99AJmZk@cluster0.0f5pf1v.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
+// const uri =
+// "mongodb+srv://vercel-admin-user:H6iHPF1uS99AJmZk@cluster0.0f5pf1v.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 module.exports = async (req, res) => {
@@ -11,6 +11,7 @@ module.exports = async (req, res) => {
         if (req.method === "GET") {
             const database = client.db("sample_mflix");
             const getMovies = database.collection("movies");
+            console.log(getMovies);
             const movies = await getMovies.find({}).toArray();
             res.status(200).json({ movies });
         } else {
