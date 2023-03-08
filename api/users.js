fa3179f9354b1e2246147a85915c73d0b8e14c3e
Thomas Vickers <vickerst10@gmail.com>
2023-03-07 18:05:39 -0700

    fix database5

--- api/users.js
+++ api/users.js
@@ -3,7 +3,7 @@ import { connectToDatabase } from "../lib/database";
 module.exports = async (req, res) => {
     if (req.method === "GET") {
         const db = await connectToDatabase();
-        const collection = await db.collection("listingsAndReviews");
+        const collection = await db.collection("movies");
 
         const users = await collection.find({}).toArray();
 
